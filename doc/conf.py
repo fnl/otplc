5bde0621631747987743bdf1b065d52195f3161f
Florian Leitner <florian.leitner@gmail.com>
2014-10-17 16:30:55 +0200

    use .* instead of _* for the dynamic Sphinx directories commented out templates and static (unused)

--- doc/conf.py
+++ doc/conf.py
@@ -34,7 +34,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['.templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -70,7 +70,7 @@ release = '1.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['.build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -130,7 +130,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['.static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
